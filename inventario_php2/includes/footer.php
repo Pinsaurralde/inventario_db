16a77001a5b2d034aa9121392b8d0ea8c65e4288
Pablo Insaurralde <insaupablopn@gmail.com>
2025-09-09 17:32:50 -0300

    Delete inventario_php2 directory

--- inventario_php2/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-// includes/footer.php
-?>
-    </div> <footer class="footer">
-        <p>&copy; <?php echo date('Y'); ?> Sistema de Inventario. Todos los derechos reservados.</p>
-    </footer>
-
-    </body>
-</html>
--- inventario_php2/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// index.php
-// Redirige directamente a la página de login
-header("Location: login.php");
-exit();
-?>
